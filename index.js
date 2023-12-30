c0bc148c5c383cf0bc24b93963f55f6b94b5d90b
angelinatrnjy <angelinatrunajaya@icloud.com>
2023-12-30 23:59:47 +0700

    Menyambungkan scatter plot ke database

--- index.js
+++ index.js
@@ -1,121 +1,120 @@
-import express from 'express';
-import csvParser from 'csv-parser';
-import multer from 'multer';
-import fs from 'fs';
+import express from "express";
+import csvParser from "csv-parser";
+import multer from "multer";
+import fs from "fs";
+import bodyParser from "body-parser";
 
 const PORT = 8080;
 const app = express();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
 app.listen(PORT, () => {
-    console.log(`Server is ready, listening on port ${PORT}`);
+  console.log(`Server is ready, listening on port ${PORT}`);
 });
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 //dipakai untuk page see report
 app.use(express.urlencoded({ extended: true }));
 //dipakai untuk routing path file css, assets
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.json());
 
 //DATABASE CONNECTION--------------------------------------------------------------------------------
-import mysql from 'mysql'
+import mysql from "mysql";
 
 //perlu dibuat terlebih dahulu database di phpmyadmin dengan nama "retaildash", lalu import file table structure.sql
 const pool = mysql.createPool({
-    user: 'root',
-    password: '',
-    database: 'retaildash',
-    host: 'localhost'
-
+  user: "root",
+  password: "",
+  database: "retaildash",
+  host: "localhost",
 });
 
 const dbConnect = () => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, conn) => {
-            if(err){
-                reject (err);
-            }
-            else{
-                resolve(conn);
-            }
-        }
-        )
-    })
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, conn) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(conn);
+      }
+    });
+  });
 };
 
 const conn = await dbConnect(); // Connect to the database
 //============================================================================================
 
 //page yang pertama kali muncul (home)
-app.get('/', (req, res)=>{
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 //ADD DATA-----------------------------------------------------------------------
 
 //saat "Add Data" di klik
-app.get('/add_data', (req, res)=>{
-    res.render('add_data')
-})
+app.get("/add_data", (req, res) => {
+  res.render("add_data");
+});
 
-app.post('/uploadFile', upload.single('file_upload'), (req, res) => {
-    const csvFile = req.file;
- 
-    pool.getConnection((err, conn) => {
-      if (err) {
-        console.error('Error connecting to database:', err);
-        res.status(500).send('Error connecting to database');
-        return;
-      }
-  
-      fs.createReadStream(csvFile.path)
-        .pipe(csvParser())
-        .on('data', (row) => {
-          const retailMarketing = {
-            ID: row.ID,
-            Year_Birth: row.Year_Birth,
-            Education: row.Education,
-            Marital_Status: row.Marital_Status,
-            Income: row.Income,
-            Kidhome: row.Kidhome,
-            Teenhome: row.Teenhome,
-            Dt_Customer: row.Dt_Customer,
-            Recency: row.Recency,
-            MntWines: row.MntWines,
-            MntFruits: row.MntFruits,
-            MntMeatProducts: row.MntMeatProducts,
-            MntFishProducts: row.MntFishProducts,
-            MntSweetProducts: row.MntSweetProducts,
-            MntGoldProds: row.MntGoldProds,
-            NumDealsPurchases: row.NumDealsPurchases,
-            NumWebPurchases: row.NumWebPurchases,
-            NumCatalogPurchases: row.NumCatalogPurchases,
-            NumStorePurchases: row.NumStorePurchases,
-            NumWebVisitsMonth: row.NumWebVisitsMonth,
-            AcceptedCmp3: row.AcceptedCmp3,
-            AcceptedCmp4: row.AcceptedCmp4,
-            AcceptedCmp5: row.AcceptedCmp5,
-            AcceptedCmp1: row.AcceptedCmp1,
-            AcceptedCmp2: row.AcceptedCmp2,
-            Complain: row.Complain,
-            Z_CostContact: row.Z_CostContact,
-            Z_Revenue: row.Z_Revenue,
-            Response: row.Response
-          };
-  
-          const query = 'INSERT INTO retail_marketing SET ?';
-          conn.query(query, retailMarketing, (error, results, fields) => {
-            if (error) {
-              console.error('Error importing data to Retail Marketing table:', error);
-            }
-          });
-        })
-        .on('end', () => {
-          conn.release();
-          res.redirect('/add_data_conf');
+app.post("/uploadFile", upload.single("file_upload"), (req, res) => {
+  const csvFile = req.file;
+
+  pool.getConnection((err, conn) => {
+    if (err) {
+      console.error("Error connecting to database:", err);
+      res.status(500).send("Error connecting to database");
+      return;
+    }
+
+    fs.createReadStream(csvFile.path)
+      .pipe(csvParser())
+      .on("data", (row) => {
+        const retailMarketing = {
+          ID: row.ID,
+          Year_Birth: row.Year_Birth,
+          Education: row.Education,
+          Marital_Status: row.Marital_Status,
+          Income: row.Income,
+          Kidhome: row.Kidhome,
+          Teenhome: row.Teenhome,
+          Dt_Customer: row.Dt_Customer,
+          Recency: row.Recency,
+          MntWines: row.MntWines,
+          MntFruits: row.MntFruits,
+          MntMeatProducts: row.MntMeatProducts,
+          MntFishProducts: row.MntFishProducts,
+          MntSweetProducts: row.MntSweetProducts,
+          MntGoldProds: row.MntGoldProds,
+          NumDealsPurchases: row.NumDealsPurchases,
+          NumWebPurchases: row.NumWebPurchases,
+          NumCatalogPurchases: row.NumCatalogPurchases,
+          NumStorePurchases: row.NumStorePurchases,
+          NumWebVisitsMonth: row.NumWebVisitsMonth,
+          AcceptedCmp3: row.AcceptedCmp3,
+          AcceptedCmp4: row.AcceptedCmp4,
+          AcceptedCmp5: row.AcceptedCmp5,
+          AcceptedCmp1: row.AcceptedCmp1,
+          AcceptedCmp2: row.AcceptedCmp2,
+          Complain: row.Complain,
+          Z_CostContact: row.Z_CostContact,
+          Z_Revenue: row.Z_Revenue,
+          Response: row.Response,
+        };
+
+        const query = "INSERT INTO retail_marketing SET ?";
+        conn.query(query, retailMarketing, (error, results, fields) => {
+          if (error) {
+            console.error("Error importing data to Retail Marketing table:", error);
+          }
         });
       })
+      .on("end", () => {
+        conn.release();
+        res.redirect("/add_data_conf");
+      });
   });
+});
 
 //saat data dari csv sukses diinput ke database
 app.get("/add_data_conf", (req, res) => {
@@ -153,36 +152,57 @@ app.post("/searched_report", async (req, res) => {
 });
 
 //saat "Bar Chart" di klik
-app.get('/bar_chart', (req, res)=>{
-    pool.getConnection((err, conn) => {
+app.get("/bar_chart", (req, res) => {
+  pool.getConnection((err, conn) => {
+    if (err) {
+      console.error("Error connecting to database:", err);
+      res.status(500).send("Error connecting to database");
+      return;
+    }
+    const sql = "SHOW COLUMNS FROM retail_marketing";
+    conn.query(sql, (err, result) => {
       if (err) {
-        console.error('Error connecting to database:', err);
-        res.status(500).send('Error connecting to database');
-        return;
+        reject(err);
+      } else {
+        res.render("bar_chart", {
+          data: result,
+        });
       }
-      const sql = 'SHOW COLUMNS FROM retail_marketing';
-      conn.query(sql, (err, result) => {
-          if (err) {
-              reject(err);
-          } else {
-              res.render('bar_chart', {
-                data: result
-              });
-          }
-      });
-    })
-    conn.release();
-})
+    });
+  });
+  conn.release();
+});
 
 //saat "Scatter Plot" di klik
-app.get('/scatter_plot', (req, res)=>{
-  res.render('scatter_plot')
-})
+app.get("/scatter_plot", (req, res) => {
+  res.render("scatter_plot");
+});
 
 //server side rendering untuk scatter plot
-app.post('/scatterPlot', (req, res) => {
+app.post("/scatterPlot", async (req, res) => {
+  const { xAxis, yAxis } = req.body;
 
-  // const data = req.body; //data yang dikirim dari formulir di halaman HTML
+  console.log(req.body);
 
-  res.json({ message: 'Scatter plot data received successfully.' });
+  console.log("xAxis:", xAxis);
+  console.log("yAxis:", yAxis);
+
+  const conn = await dbConnect();
+  const scatterData = await getScatterData(conn, xAxis, yAxis);
+
+  res.json({ scatterData, xAxis, yAxis });
 });
+
+const getScatterData = (conn, xAxis, yAxis) => {
+  return new Promise((resolve, reject) => {
+    // Lakukan query ke database untuk mendapatkan data scatter plot
+    const query = `SELECT ${xAxis} AS x, ${yAxis} AS y FROM retail_marketing`;
+    conn.query(query, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
